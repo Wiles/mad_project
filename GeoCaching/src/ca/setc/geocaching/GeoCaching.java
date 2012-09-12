fcf1b7aca19550426a4224e859c4ad69e5045897
tkempton <kempton.tom@gmail.com>
2012-09-12 11:40:15 -0400

    added retarded splash screen

--- GeoCaching/src/ca/setc/geocaching/GeoCaching.java
+++ GeoCaching/src/ca/setc/geocaching/GeoCaching.java
@@ -1,20 +1,47 @@
 package ca.setc.geocaching;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 
 public class GeoCaching extends Activity {
 
+	protected Dialog mSplashDialog;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geo_caching);
+        showSplashScreen();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_geo_caching, menu);
         return true;
+    }
+    
+    protected void removeSplashScreen() {
+        if (mSplashDialog != null) {
+            mSplashDialog.dismiss();
+            mSplashDialog = null;
+        }
+    }
+    
+    protected void showSplashScreen() {
+        mSplashDialog = new Dialog(this);
+        mSplashDialog.setContentView(R.layout.splashscreen);
+        mSplashDialog.setCancelable(false);
+        mSplashDialog.show();
+         
+        // Set Runnable to remove splash screen just in case
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+          public void run() {
+            removeSplashScreen();
+          }
+        }, 3000);
     }
 }
