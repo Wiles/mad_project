ad2a1576758b1c70ee6f67c88cd7ad89b022bee5
Samuel Lewis <samuelewis@gmail.com>
2012-09-19 11:25:18 -0400

    Quick hack that provides an example of GPS usage

--- GeoCaching/src/ca/setc/geocaching/GeoCaching.java
+++ GeoCaching/src/ca/setc/geocaching/GeoCaching.java
@@ -1,16 +1,23 @@
 package ca.setc.geocaching;
 
 import android.app.Dialog;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 
 public class GeoCaching extends MapActivity {
 
 	protected Dialog mSplashDialog;
+	protected MapController mc;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,9 @@ public class GeoCaching extends MapActivity {
         setContentView(R.layout.activity_geo_caching);
         MapView mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
+        mc = mapView.getController();
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L, 500.0f, new LL());
     }
 
     @Override
@@ -53,5 +63,30 @@ public class GeoCaching extends MapActivity {
 	protected boolean isRouteDisplayed() {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	private class LL implements LocationListener {
+
+		public void onLocationChanged(Location location) {
+			
+			mc.setCenter(new GeoPoint((int)(location.getLatitude() * 1e6), (int)(location.getLongitude() * 1e6)));
+			
+		}
+
+		public void onProviderDisabled(String provider) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void onProviderEnabled(String provider) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			// TODO Auto-generated method stub
+			
+		}
+		
 	}
 }
